25c22eea99ad690343e62149229b1c80e24b6dd9
Tynahan <nusilite@gmail.com>
2025-08-05 16:12:40 +0900

    ✨ Feat(error): Handle env errors

--- server.ts
+++ server.ts
@@ -21,13 +21,26 @@ app.get("/", (req, res) => res.send("Welcome!"))
 
 app.post("/tmdb", async (req, res) => {
     try {
+        if (!tmdbAccessToken) {
+            console.error("---- env var missing")
+            throw new Error("---- Unknwon error")
+        }
+
         const url = req.body
-        
+
         const response = await axios.get(url, { headers: { "Authorization": `Bearer ${tmdbAccessToken}` } })
         const json = response.data.results ? response.data.results : response.data
         res.status(200).json(json)
     } catch (error: any) {
-        res.status(400).json({message: "----ERROR OCCURRED!", error})
+        // 삭제 금지 삭제 금지 삭제 금지 삭제 금지 삭제 금지 
+        console.error("Full error:", {
+            message: error.message,
+            response: error.response?.data,
+            status: error.response?.status,
+            url: error.config?.url
+        })
+        // 삭제 금지 삭제 금지 삭제 금지 삭제 금지 삭제 금지       
+        res.status(400).json({ message: "----ERROR OCCURRED!", error })
     }
 })
 
